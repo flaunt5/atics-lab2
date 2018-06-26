2e9e1167124a6e01ced6550035e10619ce992c3f
Joris Brandt <jorisbrandt@gmail.com>
2018-06-26 23:56:23 +0200

    more

--- base.R
+++ base.R
@@ -9,3 +9,23 @@ num_classes <- count_params(y_train)
 tokenizer <- text_tokenizer(num_words = 1000)
 x_train <- sequences_to_matrix(tokenizer, x_train, mode = 'binary')
 x_test <- sequences_to_matrix(tokenizer, x_test, mode = 'binary')
+
+model_cifar1 <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu', input_shape = c(32, 32, 3), padding = "same") %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu') %>%
+  layer_max_pooling_2d(pool_size =  c(2,2)) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu', padding = "same") %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu') %>%
+  layer_max_pooling_2d(pool_size =  c(2,2)) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_flatten() %>%
+  layer_dense(units = 512, activation = 'relu') %>%
+  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 10, activation = 'softmax')
+
+model_cifar1 %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(lr = 0.0001, decay = 1e-6),
+  metrics = c('accuracy')
+)
